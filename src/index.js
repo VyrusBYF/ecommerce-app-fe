cadf209a495bb5d0652a7f4742a9958137efa966
Richard Howlett <richardhowlett14@gmail.com>
2023-04-26 14:15:50 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./_shared/brand";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.min.css";
 import "react-bootstrap";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -17,10 +17,10 @@ root.render(
   <React.StrictMode>
     <CookiesProvider>
       <ShoppingCartProvider>
-        <Router basename={process.env.PROCESS_URL}>
+        <HashRouter basename={process.env.PROCESS_URL}>
           <ScrollToTop />
           <App />
-        </Router>
+        </HashRouter>
       </ShoppingCartProvider>
     </CookiesProvider>
   </React.StrictMode>
